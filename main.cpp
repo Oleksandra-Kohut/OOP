bf3e52db4af03ae3217656ba0b6a1255fb827fa8
Oleksandra <oleksandrakogut71@gmail.com>
2026-03-23 20:36:11 +0200

    laba1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
-#include "cat.h"
 #include "dog.h"
+#include "cat.h"
 #include "bird.h"
 
-    using namespace std;
+using namespace std;
 
-    int main() {
+int main() {
 
-        Dog d1("Rex", 3);
-        Cat c1("Murka", "gray");
-        Bird b1("Eagle");
+    Dog d1("Rex", 3);
+    d1.bark();
 
-        d1.bark();
-        c1.meow();
-        b1.fly();
+    Cat c1("Murka", "gray");
+    c1.meow();
 
-        return 0;
-    }
+    Cat c2 = c1;
+
+    Cat c3 = Cat("Barsik", "white");
+
+    c3.meow();
+
+    c1.setName("Tom");
+    c1.meow();
+
+    const Cat c4("ConstCat", "black");
+    c4.meow();
+
+    cout << "Cats created: " << Cat::getCount() << endl;
+
+    Cat c5 = -c1;
+    cout << c5 << endl;
+
+    Cat c6 = c1 + c3;
+    cout << c6 << endl;
+
+    Cat c7;
+    cin >> c7;
+    cout << c7 << endl;
+
+    Bird b1("Eagle");
+    b1.fly();
+
+
+    return 0;
+}
